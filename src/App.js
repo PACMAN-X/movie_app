9183cc52ea2a13fc2a15e808cc6b04ab2f688eea
sr9872@naver.com <sr9872@naver.com>
2019-12-20 00:56:26 +0900

    3.3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log("constructed!")
+  }
   state = { count: 0 }
+  componentDidMount() {
+    console.log("didMount!")
+  }
+  componentDidUpdate() {
+    console.log("didUpdate!")
+  }
   add = () => this.setState(current => ({ count: current.count + 1 }))
   minus = () => this.setState(current => ({ count: current.count - 1 }))
   render() {
+    console.log("render!")
     return <div>
       <h1>The number is: {this.state.count}</h1>
       <button onClick={this.add} >Add</button>
