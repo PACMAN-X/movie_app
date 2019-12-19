d64e69e0606e7337d9bb58d376ae8ee98d0fef33
sr9872@naver.com <sr9872@naver.com>
2019-12-20 01:07:29 +0900

    3.4

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
-import React from 'react';
+import React from 'react'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    console.log("constructed!")
+  state = {
+    isLoading: true
   }
-  state = { count: 0 }
   componentDidMount() {
-    console.log("didMount!")
+    setTimeout(() => {
+      this.setState({ isLoading: false })
+    }, 6000);
   }
-  componentDidUpdate() {
-    console.log("didUpdate!")
-  }
-  add = () => this.setState(current => ({ count: current.count + 1 }))
-  minus = () => this.setState(current => ({ count: current.count - 1 }))
   render() {
-    console.log("render!")
-    return <div>
-      <h1>The number is: {this.state.count}</h1>
-      <button onClick={this.add} >Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div >
+    const { isLoading } = this.state
+    return <div><h1>{isLoading ? "Loading...." : "We are Ready!"}</h1></div>
   }
 }
 
-export default App;
+export default App
